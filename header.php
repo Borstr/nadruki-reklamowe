fef4e4615c8513a37651158ab60a92426aaa4b35
Borys Strzelewicz <borstr@wp.pl>
2020-03-12 00:50:47 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap&subset=latin-ext" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+    <header class="topheader">
+        <aside class="topheader__socials">
+        
+        </aside>
+        <div class="topheader__menu">
+            <ul class="topheader__menulist">
+                <li>
+                    <button class="topheader__offer" aria-label="otwórz menu">Oferta</button>
+                </li>
+                <li class="topheader__title">Nadruki Reklamowe</li>
+            </ul>
+        </div>
+        <nav class="topheader__nav">
+            <ul class="topheader__navlist">
+                <li>
+                    <a class="topheader__navitem" href="/">Strona główna</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/dlugopisy.html">Długopisy</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/balony.html">Balony</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/torby-papierowe-z-nadrukiem.html">Torby papierowe</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/reklamowki.html">Reklamówki</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/torby-bawelniane-materialowe.html">Torby bawełniane, materiałowe</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/koszulki-z-nadrukiem.html">Koszulki</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/koszulki-damskie-z-nadrukiem.html">Koszulki damskie</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/koszulki-dzieciece.html">Koszulki dziecięce</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/bluzy.html">Bluzy</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/kurtki.html">Kurtki</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/czapki-z-daszkiem.html">Czapki z daszkiem</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/przypinki.html">Przypinki, buttony, badziki</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/podkladki.html">Podkładki</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/kubki.html">Kubki</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/parasole.html">Parasole</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/otwieracze.html">Otwieracze</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/puzzle.html">Puzzle</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/zapalniczki.html">Zapalniczki</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/kontakt.html">Kontakt</a>
+                </li>
+                <li>
+                    <a class="topheader__navitem" href="/blog">Blog</a>
+                </li>
+            </ul>
+        </nav>
+        <button class="topheader__background" aria-label="close menu"></button>
+    </header>
+<body>

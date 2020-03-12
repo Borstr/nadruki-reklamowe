7cb9323b335dae57b14d6229f82a990f7242ed51
Borys Strzelewicz <borstr@wp.pl>
2020-03-12 02:54:01 +0100

    added dependencies and set up for scss

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 
 function load_css() {
-    wp_register_style('global', get_template_directory_uri() . '/css/global.css', array(), false, 'all');
+    wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all');
 
-    wp_enqueue_style('global');
+    wp_enqueue_style('main');
 }
 
 add_action('wp_enqueue_scripts', 'load_css');
--- header.php
+++ header.php
@@ -4,88 +4,87 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap&subset=latin-ext" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
-    <header class="topheader">
-        <aside class="topheader__socials">
+    <header>
+        <aside class="socials">
         
         </aside>
-        <div class="topheader__menu">
-            <ul class="topheader__menulist">
+        <div class="menu">
+            <ul class="menu__list">
                 <li>
-                    <button class="topheader__offer" aria-label="otwórz menu">Oferta</button>
+                    <button class="menu__offer" aria-label="otwórz menu">Oferta</button>
                 </li>
-                <li class="topheader__title">Nadruki Reklamowe</li>
+                <li class="menu__title">Nadruki Reklamowe</li>
             </ul>
         </div>
-        <nav class="topheader__nav">
-            <ul class="topheader__navlist">
+        <nav class="sidenav">
+            <ul class="sidenav__list">
                 <li>
-                    <a class="topheader__navitem" href="/">Strona główna</a>
+                    <a class="sidenav__item" href="/">Strona główna</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/dlugopisy.html">Długopisy</a>
+                    <a class="sidenav__item" href="/dlugopisy.html">Długopisy</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/balony.html">Balony</a>
+                    <a class="sidenav__item" href="/balony.html">Balony</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/torby-papierowe-z-nadrukiem.html">Torby papierowe</a>
+                    <a class="sidenav__item" href="/torby-papierowe-z-nadrukiem.html">Torby papierowe</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/reklamowki.html">Reklamówki</a>
+                    <a class="sidenav__item" href="/reklamowki.html">Reklamówki</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/torby-bawelniane-materialowe.html">Torby bawełniane, materiałowe</a>
+                    <a class="sidenav__item" href="/torby-bawelniane-materialowe.html">Torby bawełniane, materiałowe</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/koszulki-z-nadrukiem.html">Koszulki</a>
+                    <a class="sidenav__item" href="/koszulki-z-nadrukiem.html">Koszulki</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/koszulki-damskie-z-nadrukiem.html">Koszulki damskie</a>
+                    <a class="sidenav__item" href="/koszulki-damskie-z-nadrukiem.html">Koszulki damskie</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/koszulki-dzieciece.html">Koszulki dziecięce</a>
+                    <a class="sidenav__item" href="/koszulki-dzieciece.html">Koszulki dziecięce</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/bluzy.html">Bluzy</a>
+                    <a class="sidenav__item" href="/bluzy.html">Bluzy</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/kurtki.html">Kurtki</a>
+                    <a class="sidenav__item" href="/kurtki.html">Kurtki</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/czapki-z-daszkiem.html">Czapki z daszkiem</a>
+                    <a class="sidenav__item" href="/czapki-z-daszkiem.html">Czapki z daszkiem</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/przypinki.html">Przypinki, buttony, badziki</a>
+                    <a class="sidenav__item" href="/przypinki.html">Przypinki, buttony, badziki</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/podkladki.html">Podkładki</a>
+                    <a class="sidenav__item" href="/podkladki.html">Podkładki</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/kubki.html">Kubki</a>
+                    <a class="sidenav__item" href="/kubki.html">Kubki</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/parasole.html">Parasole</a>
+                    <a class="sidenav__item" href="/parasole.html">Parasole</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/otwieracze.html">Otwieracze</a>
+                    <a class="sidenav__item" href="/otwieracze.html">Otwieracze</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/puzzle.html">Puzzle</a>
+                    <a class="sidenav__item" href="/puzzle.html">Puzzle</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/zapalniczki.html">Zapalniczki</a>
+                    <a class="sidenav__item" href="/zapalniczki.html">Zapalniczki</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/kontakt.html">Kontakt</a>
+                    <a class="sidenav__item" href="/kontakt.html">Kontakt</a>
                 </li>
                 <li>
-                    <a class="topheader__navitem" href="/blog">Blog</a>
+                    <a class="sidenav__item" href="/blog">Blog</a>
                 </li>
             </ul>
         </nav>
-        <button class="topheader__background" aria-label="close menu"></button>
+        <button class="sidenav__background" aria-label="close menu"></button>
     </header>
 <body>

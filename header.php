7e4e5e5e7c82318c1b23859d77a89e088edf12a8
Borys Strzelewicz <borstr@wp.pl>
2020-03-16 03:13:56 +0100

    changed footer and front-page and added youtube to socials

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
                 <a rel="noopener norefferer nofollow" target="_blank" href="https://www.facebook.com/balonyznadrukiem"><img src="/wordpress/wp-content/themes/nadrek/images/facebook-icon.png" alt="facebook icon" class="socials__icon"></a>
                 <a rel="noopener norefferer nofollow" href="#"><img src="/wordpress/wp-content/themes/nadrek/images/instagram-icon.png" alt="instagram icon" class="socials__icon"></a>
                 <a rel="noopener norefferer nofollow" href="#"><img src="/wordpress/wp-content/themes/nadrek/images/linkedin-icon.png" alt="linkedin icon" class="socials__icon"></a>
+                <a rel="noopener norefferer nofollow" href="https://www.youtube.com/channel/UCaKZLi3IEPZX-DRi5R83PVg"><img src="/wordpress/wp-content/themes/nadrek/images/youtube-icon.png" alt="youtube icon" class="socials__icon"></a>
             </div>
             <div class="socials__contact">
                 <a href="tel:52 381 81 41">zadzwoń <span class="socials__expanded">- 52 381 81 41</span></a>

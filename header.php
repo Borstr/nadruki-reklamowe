5315266e3a946333f4e676757a25a47a25b2d40d
Borys Strzelewicz <borstr@wp.pl>
2020-04-05 09:13:49 +0200

    changed social icons to svgs

--- header.php
+++ header.php
@@ -10,11 +10,23 @@
     <header>
         <aside class="socials" id="socials">
             <div class="socials__icons">
-                <a rel="noopener norefferer nofollow" target="_blank" href="https://twitter.com/NadrukiR"><img src="/blog/wp-content/uploads/2020/03/twitter-icon.png" alt="twitter icon" class="socials__icon"></a>
-                <a rel="noopener norefferer nofollow" target="_blank" href="https://www.facebook.com/balonyznadrukiem"><img src="/blog/wp-content/uploads/2020/03/facebook-icon.png" alt="facebook icon" class="socials__icon"></a>
+                <a rel="noopener norefferer nofollow" target="_blank" href="https://twitter.com/NadrukiR">
+                    <svg class="socials__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 203.14">
+                        <path fill="#1da1f2" d="M78.62,203.14C173,203.14,224.56,125,224.56,57.2c0-2.22,0-4.43-.15-6.63A104.36,104.36,0,0,0,250,24a102.24,102.24,0,0,1-29.46,8.07A51.46,51.46,0,0,0,243.09,3.72a103,103,0,0,1-32.57,12.45A51.34,51.34,0,0,0,123.11,63,145.63,145.63,0,0,1,17.4,9.36,51.33,51.33,0,0,0,33.28,77.83,51,51,0,0,1,10,71.41v.65a51.32,51.32,0,0,0,41.15,50.28,51.2,51.2,0,0,1-23.16.88,51.36,51.36,0,0,0,47.92,35.62,103,103,0,0,1-63.7,22A106,106,0,0,1,0,180.1a145.22,145.22,0,0,0,78.62,23"/>
+                    </svg>
+                </a>
+                <a rel="noopener norefferer nofollow" target="_blank" href="https://www.facebook.com/balonyznadrukiem">
+                    <svg class="socials__icon" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="14.2222in" height="14.2222in" version="1.1" viewBox="0 0 14222 14222" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <path fill="#1977F3" d="M14222 7111c0,-3927 -3184,-7111 -7111,-7111 -3927,0 -7111,3184 -7111,7111 0,3549 2600,6491 6000,7025l0 -4969 -1806 0 0 -2056 1806 0 0 -1567c0,-1782 1062,-2767 2686,-2767 778,0 1592,139 1592,139l0 1750 -897 0c-883,0 -1159,548 -1159,1111l0 1334 1972 0 -315 2056 -1657 0 0 4969c3400,-533 6000,-3475 6000,-7025z"/>
+                    </svg>
+                </a>
                 <!-- <a rel="noopener norefferer nofollow" href="#"><img src="/blog/wp-content/uploads/2020/03/instagram-icon.png" alt="instagram icon" class="socials__icon"></a> -->
                 <!-- <a rel="noopener norefferer nofollow" href="#"><img src="/blog/wp-content/uploads/2020/03/linkedin-icon.png" alt="linkedin icon" class="socials__icon"></a> -->
-                <a rel="noopener norefferer nofollow" href="https://www.youtube.com/channel/UCaKZLi3IEPZX-DRi5R83PVg"><img src="/blog/wp-content/uploads/2020/03/youtube-icon.png" alt="youtube icon" class="socials__icon"></a>
+                <a rel="noopener norefferer nofollow" target="_blank" href="https://www.youtube.com/channel/UCaKZLi3IEPZX-DRi5R83PVg">
+                    <svg class="socials__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 176 124">
+                        <path fill="red" d="M172.32,19.36A22.12,22.12,0,0,0,156.76,3.7C143,0,88,0,88,0S33,0,19.24,3.7A22.12,22.12,0,0,0,3.68,19.36C0,33.18,0,62,0,62s0,28.82,3.68,42.64A22.12,22.12,0,0,0,19.24,120.3C33,124,88,124,88,124s55,0,68.76-3.7a22.12,22.12,0,0,0,15.56-15.66C176,90.82,176,62,176,62S176,33.18,172.32,19.36ZM70,88.17V35.83L116,62Z"/>
+                    </svg>
+                </a>
             </div>
             <div class="socials__contact">
                 <a href="tel:523818141">zadzwoń <span class="socials__expanded">- 52 381 81 41</span></a>
@@ -22,23 +34,16 @@
             </div>
         </aside>
         <div class="menu" id="menu">
-                <button class="menu__offer" id="menuBtn" aria-label="otwórz menu">
-                    Oferta 
-                    <svg xmlns="http://www.w3.org/2000/svg" id="chevron" class="chevron" viewBox="0 0 10.349 17.743">
-                        <path 
-                            d="M1990,138.468l6.727,6.75-6.727,6.75" 
-                            transform="translate(-1987.879 -136.346)" 
-                            fill="none" 
-                            stroke="#f0c05a" 
-                            stroke-linecap="round" 
-                            stroke-width="3"
-                        />
-                    </svg>
-                </button>
-                <a href="/" class="menu__title">
-                    Nadruki Reklamowe
-                    <img src="/blog/wp-content/uploads/2020/03/ikona-nadruki-1.png" alt="nadruki reklamowe logo" class="menu__logo">
-                </a>
+            <button class="menu__offer" id="menuBtn" aria-label="otwórz menu">
+                Oferta 
+                <svg xmlns="http://www.w3.org/2000/svg" id="chevron" class="chevron" viewBox="0 0 10.349 17.743">
+                    <path d="M1990,138.468l6.727,6.75-6.727,6.75" transform="translate(-1987.879 -136.346)" fill="none" stroke="#f0c05a" stroke-linecap="round" stroke-width="3" />
+                </svg>
+            </button>
+            <a href="/" class="menu__title">
+                Nadruki Reklamowe
+                <img src="/blog/wp-content/uploads/2020/03/ikona-nadruki-1.png" alt="nadruki reklamowe logo" class="menu__logo">
+            </a>
         </div>
         <nav class="sidenav" id="nav">
             <ul class="sidenav__list">
